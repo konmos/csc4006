d2e49636143182e2d28cf4c250651bb659b37009
Konrad <kojak334@gmail.com>
2022-01-07 19:12:06 +0000

    Add proof of concept

---
+++ analyse/analyse.py
@@ -0,0 +1,39 @@
+import string
+
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+
+TAGGER = SequenceTagger.load('ner-large')
+
+
+def clean_text(text: str) -> str:
+    text = text.translate(
+        str.maketrans({
+            '”': '"', '“': '"',
+            '’': '\'',
+            '…': '...'
+        })
+    )
+
+    return [x.strip() for x in text.split('.')]  # extract sentences
+
+
+def extract_entities(text):
+    sentences, entities = clean_text(text), []
+
+    for s in sentences:
+        sent = Sentence(s)
+        TAGGER.predict(sent)
+
+        for entity in sent.to_dict(tag_type='ner')['entities']:
+            entities.append(entity['text'])
+
+    return set([
+        x.translate(str.maketrans('', '', string.punctuation)) for x in entities
+    ])
+
+
+
+with open('byzantine.txt') as fd:
+    print(extract_entities(fd.read()))
