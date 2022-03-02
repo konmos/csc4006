9a41c6a1efdd23b852391ca59b7f46a51aae5d59
Konrad <kojak334@gmail.com>
2022-03-02 17:49:11 +0000

    Add step command

--- formalisation/runner.py
+++ formalisation/runner.py
@@ -1,5 +1,6 @@
 import click
 import importlib
+from typing import get_type_hints
 
 from formalisation.formalisation import World, visualize_fdl
 
@@ -49,6 +50,75 @@ def run(fname, origin=None, ix=False, trace=None, unique=False, fdl=None):
 
     if trace is not None:
         world.draw_trace_graph(notebook=False, fname=trace, unique_events=unique)
+
+
+class LiveStepper:
+    def __init__(self, world):
+        self.world = world
+        self.exit = False
+
+    def parse_command(self, command):
+        cmd, *args = [x.strip() for x in command.split(' ')]
+
+        try:
+            meth = getattr(self, f'cmd_{cmd}')
+        except AttributeError:
+            return 'Invalid Command.'
+
+        type_hints = list(get_type_hints(meth).values())
+
+        for i in range(len(args)):
+            if i < len(type_hints):
+                args[i] = type_hints[i](args[i])
+
+        return meth(*args)
+
+    def cmd_help(self):
+        return (
+            'Available Commands:\n'
+            '  exit: Stop stepping and exit the program.\n'
+            '  step [X]: Make X steps (default = 1).\n'
+            '  trace: Return current raw trace.\n'
+            '  fdl: Return the FDL for this world.\n'
+            '  trace_graph [unique]: Draw the current trace.\n'
+            '  fdl_graph: Draw the FDL for this world.'
+        )
+
+    def cmd_exit(self):
+        self.exit = True
+
+    def cmd_step(self, by: int = 1):
+        ret = False
+
+        for _ in range(by):
+            ret = self.world.step()
+
+        return ret
+
+    def cmd_trace(self):
+        return self.world._last_trace
+
+    def cmd_fdl(self):
+        return '\n'.join(self.world.generate_fdl())
+
+    def cmd_trace_graph(self, *args):
+        self.world.draw_trace_graph(
+            notebook=False,
+            unique_events='unique' in args
+        )
+
+    def cmd_fdl_graph(self):
+        self.world.draw_fdl_graph(notebook=False)
+
+
+@cli.command()
+@click.argument('fname')
+def step(fname):
+    world = _get_world(fname)
+    stepper = LiveStepper(world)
+
+    while not stepper.exit:
+        print(stepper.parse_command(input('> ')))
 
 
 if __name__ == '__main__':
